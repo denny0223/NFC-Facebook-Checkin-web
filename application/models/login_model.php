d2dffa7382a5233d88531e6019643f40f434fb15
DennyHuang <denny0223@gmail.com>
2013-05-05 21:22:34 +0800

    forgot add login_model to commit

---
+++ application/models/login_model.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Login_model extends CI_Model {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function isUserValid($username, $password)
+	{
+		$sql = "SELECT `username`
+				FROM `account` 
+				WHERE `username` = ?
+				AND `password` = MD5(CONCAT(MD5(?), 'nfccheckin'))";
+
+		$query = $this->db->query($sql , array($username, $password));
+
+		return $query->num_rows();
+	}
+
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
