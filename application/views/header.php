2c65a50c5fa348b785c91aa0a196cb271d4aafd7
DennyHuang <denny0223@gmail.com>
2013-05-08 15:46:38 +0800

    For login user header file init

---
+++ application/views/header.php
@@ -0,0 +1,7 @@
+<div id="header">
+	<?php echo form_open('logout'); ?>
+	<div id="logout" align="right">
+		<input type="submit" name="logout" value="Logout">
+	</div>
+	<?php echo form_close(); ?>
+</div>
