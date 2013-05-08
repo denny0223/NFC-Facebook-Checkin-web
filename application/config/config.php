9735df0c64afbbb93ef378c565519774a5412424
DennyHuang <denny0223@gmail.com>
2013-05-08 14:36:31 +0800

    Rename session cookie name

--- application/config/config.php
+++ application/config/config.php
@@ -244,7 +244,7 @@ $config['encryption_key'] = '7884154aed7d04ac36c7e870cc7873c6';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
+$config['sess_cookie_name']		= 'session';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= TRUE;
