e0ed6495237d25015d15c423612ca8e40318ac72
DennyHuang <denny0223@gmail.com>
2013-05-09 01:01:36 +0800

    header load form helper itself

--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,12 @@
 <div id="header">
-	<?php echo form_open('logout'); ?>
+<?php 
+	$this->load->helper('form');
+	echo form_open('logout');
+?>
 	<div id="logout" align="right">
 		<input type="submit" name="logout" value="Logout">
 	</div>
-	<?php echo form_close(); ?>
+<?php
+	echo form_close();
+?>
 </div>
