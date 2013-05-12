851dd2af677bccda02c57acae244517bff5c707c
DennyHuang <denny0223@gmail.com>
2013-05-13 01:13:12 +0800

    controller rootmgr store modify get data and load view OK!

--- application/controllers/rootmgr.php
+++ application/controllers/rootmgr.php
@@ -42,4 +42,36 @@ class Rootmgr extends CI_Controller {
 		$this->load->view('rootmgrStoreList', $data);
 	}
 
+	public function storemodify($storeId = null)
+	{
+		$this->load->library(array('session', 'form_validation'));
+		$this->load->helper(array('form', 'url'));
+		$this->load->model("Account_model", '', TRUE);
+		$this->load->model("Store_model", '', TRUE);
+
+		$userId = $this->session->userdata('userId');
+		$username = $this->session->userdata('username');
+
+		// Authentication
+		if(!$userId || $username != 'mgr'){
+			redirect('/login');
+		}
+
+		$cancel = $this->input->post('cancel');
+		if($cancel == 'Cancel') redirect('/rootmgr/store');
+
+		$query = $this->Store_model->getStoreInfoWithOwnerName($storeId);
+		$data = $query->row_array();
+
+		$query = $this->Account_model->getAllUsername();
+		$users = Array();
+		foreach($query->result_array() as $row){
+			array_push($users, $row['username']);
+		}
+		$data['users'] = $users;
+
+		$this->load->view('rootmgrStoreModify', $data);
+
+	}
+
 }
