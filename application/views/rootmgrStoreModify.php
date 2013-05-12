168bc2ee6e4b9facde802f253bb1e84f5c2c436d
DennyHuang <denny0223@gmail.com>
2013-05-13 03:21:25 +0800

    Add js to check page id

--- application/views/rootmgrStoreModify.php
+++ application/views/rootmgrStoreModify.php
@@ -15,6 +15,23 @@
 				});
 			});
 		</script>
+		<script>
+			function ref(){
+				$.get("https://graph.facebook.com/"+$("#page_id")[0].value, function(a) {
+					if(a.likes === undefined){
+						$("#store_name")[0].textContent = "";
+						alert('Not a page ID');
+						return;
+					}
+					$("#store_avatar")[0].src="https://graph.facebook.com/" + $("#page_id")[0].value + "/picture?width=25&height=25";
+					$("#store_name")[0].textContent=a.name;
+				})
+				.fail(function() {
+					$("#store_name")[0].textContent = "";
+					alert("ID not available");
+				});
+			}
+		</script>
 	</head>
 	<body>
 
@@ -31,11 +48,11 @@
 						<table border="0">
 							<tr>
 								<td>
-									<img src="https://graph.facebook.com/<?php echo $page_id; ?>/picture?width=25&height=25" />
+									<img id="store_avatar" src="https://graph.facebook.com/<?php echo $page_id; ?>/picture?width=25&height=25" />
 								</td>
 								<td>
-									<?php echo anchor_popup('http://www.facebook.com/' . $page_id,
-													htmlspecialchars($store_name)); ?>
+									<?php echo anchor('http://www.facebook.com/' . $page_id,
+													htmlspecialchars($store_name), 'target="_blank" id="store_name"'); ?>
 								</td>
 							</tr>
 						</table>
@@ -44,7 +61,8 @@
 				<tr>
 					<th>粉絲團ID</th>
 					<td align="center">
-						<input type="text" name="page_id" value="<?php echo htmlspecialchars($page_id); ?>">
+						<input id="page_id" type="text" name="page_id" value="<?php echo htmlspecialchars($page_id); ?>">
+						<input type="button" name="some_name" value="Refresh" onclick="ref();">
 					</td>
 				</tr>
 				<tr>
